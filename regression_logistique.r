3a70f5eed223f84c8827943b7513b0bb357b698f
maxenceLIOGIER <maxence.liogier38@gmail.com>
2024-11-12 21:27:35 +0100

    méthode $fit(X, y)

--- regression_logistique.r
+++ regression_logistique.r
@@ -1,20 +1,19 @@
 library(R6)
-library(devtools)
+# library(devtools)
 
 # Définition de la classe LogisticRegression
 LogisticRegression <- R6Class("LogisticRegression",
   public = list(
-    initialize = function(X, y, alpha = 0.01, num_iters = 1000) {
-      #' @param X : matrice des caractéristiques
-      #' @param y : vecteur des étiquettes
+    theta = NULL,
+    nb_iters = NULL,
+    alpha = NULL,
+
+    # Initialisation de la classe
+    initialize = function(nb_iters = 1000, alpha = 0.01) {
+      #' @param nb_iters : nombre d'itérations
       #' @param alpha : taux d'apprentissage
-      #' @param num_iters : nombre d'itérations
-      self$X <- X
-      self$y <- y
+      self$nb_iters <- nb_iters
       self$alpha <- alpha
-      self$num_iters <- num_iters
-      self$theta <- NULL
-      self$cost_history <- NULL
     },
 
     # Fonction sigmoïde
@@ -25,34 +24,28 @@ LogisticRegression <- R6Class("LogisticRegression",
     },
 
     # Fonction de coût
-    fct_cout = function(theta) {
+    fct_cout = function(X, y, theta) {
       #' @param theta : vecteur des paramètres
       #' @return coût de la régression logistique
-      m <- length(self$y)
-      h <- self$sigmoid(self$X %*% theta)
-      cost <- (-1 / m) * sum(self$y * log(h) + (1 - self$y) * log(1 - h))
+      m <- length(y)
+      h <- self$sigmoid(X %*% theta)
+      cost <- (-1 / m) * sum(y * log(h) + (1 - y) * log(1 - h))
       return(cost)
     },
 
     # Fonction de descente de gradient
-    gradient_descent = function(X, y, theta, alpha, num_iters) {
+    gradient_descent = function(X, y, theta) {
       #' @param X : matrice des caractéristiques
       #' @param y : vecteur des étiquettes
       #' @param theta : vecteur des paramètres
-      #' @param alpha : taux d'apprentissage
-      #' @param num_iters : nombre d'itérations
       #' @return liste des paramètres optimisés et de l'historique des coûts
-      m <- length(y)
-      cost_history <- numeric(num_iters)
-
-      for (i in 1:num_iters) {
+      m <- nrow(X)
+      for (i in 1:self$nb_iters) {
         h <- self$sigmoid(X %*% theta)
-        gradient <- (1 / m) * (t(X) %*% (h - y))
-        theta <- theta - alpha * gradient
-        cost_history[i] <- self$fct_cout(theta)
+        gradient <- t(X) %*% (h - y) / m
+        theta <- theta - self$alpha * gradient
       }
-
-      return(list(theta = theta, cost_history = cost_history))
+      return(list(theta = theta))
     },
 
     # Fonction pour déterminer les variables qualitatives et quantitatives
@@ -90,40 +83,40 @@ LogisticRegression <- R6Class("LogisticRegression",
       return(X)
     },
 
-    # Régression logistique multinomiale
-    multinomial_logistic_regression = function(X, y, num_iters = 1000) {
+    # Modèle de régression logistique multinomiale
+    multinomial_logistic_regression = function(X, y) {
       #' @param X data.frame des caractéristiques
       #'          peut contenir des variables qualitatives et quantitatives
       #' @param y Vecteur des étiquettes = variable cible
-      #' @param num_iters Nombre d'itérations
       #' @return Liste des paramètres optimisés et de l'historique des coûts
+
+      # Préparation de la matrice X
+      X_new <- self$prepare_X(X)
 
       # Initialisation des paramètres
       classes_uniques <- unique(y)
       K <- length(classes_uniques)
-      n <- ncol(X)
+      n <- ncol(X_new)
       theta <- matrix(0, nrow = n, ncol = K)
       colnames(theta) <- classes_uniques
-      rownames(theta) <- colnames(X)
-
-      # Taux d'apprentissage
-      alpha <- 0.01
-
-      X_new <- self$prepare_X(X)
+      rownames(theta) <- colnames(X_new)
 
       # Optimisation des paramètres pour chaque classe
       for (k in 1:K) {
         y_k <- ifelse(y == classes_uniques[k], 1, 0) # Encodage one-hot
-        result <- self$gradient_descent(X_new, y_k, theta[,k], alpha, num_iters)
+        result <- self$gradient_descent(X_new, y_k, theta[, k])
         theta[, k] <- result$theta
       }
 
       return(theta)
+    },
+
+    # Fonction d'apprentissage
+    fit = function(X, y) {
+      self$theta <- self$multinomial_logistic_regression(X, y)
     }
   )
 )
-
-
 
 ## Exemple d'utilisation
 set.seed(123)
@@ -134,9 +127,7 @@ X <- data.frame(
   stringsAsFactors = TRUE
 )
 y <- c(0, 0, 1, 1, 2)
-alpha <- 0.01
-num_iters <- 1000
 
 model <- LogisticRegression$new()
-theta <- model$multinomial_logistic_regression(X, y, alpha, num_iters)
-print(theta)
+model$fit(X, y)
+print(model$theta)
