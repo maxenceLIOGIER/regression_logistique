4d94708f00fdf24473e50cc00cef57be0d618765
YacineAyachi <101209432+YacineAyachi@users.noreply.github.com>
2024-12-01 11:34:37 +0100

    did parallelizing the reg_multinomiale

--- R/reg_multinomiale.R
+++ R/reg_multinomiale.R
@@ -9,8 +9,13 @@
 #' @param lambda (numeric) Regularization parameter.
 #' @param l1_ratio (numeric) The ElasticNet mixing parameter, with 0 <= l1_ratio <= 1. Only used if penalty is 'elasticnet'.
 #' @return (matrix) Optimized parameters for each class.
+#' @import doParallel
+#' @import foreach
+#' @import parallel
+#' @include descente_gradient.R
 reg_multinomiale <- function(X, y, nb_iters, alpha,
                              penalty, lambda, l1_ratio) {
+  
   X_new <- as.matrix(prepare_x(X))
   classes_uniques <- unique(y)
   K <- length(classes_uniques)
@@ -18,8 +23,13 @@ reg_multinomiale <- function(X, y, nb_iters, alpha,
   theta <- matrix(0, nrow = n, ncol = K)
   colnames(theta) <- classes_uniques
   rownames(theta) <- colnames(X_new)
-
-  for (k in 1:K) {
+  
+  # Register parallel backend
+  cl <- parallel::makeCluster(detectCores() - 1) # Use all cores except one
+  doParallel::registerDoParallel(cl)
+  
+  # Parallelize the loop over classes
+  theta_list <- foreach(k = 1:K, .combine = cbind, .export = "descente_gradient") %dopar% {
     y_k <- ifelse(y == classes_uniques[k], 1, 0)
     result <- descente_gradient(X_new, y_k, theta[, k, drop = FALSE],
                                 nb_iters = nb_iters,
@@ -27,8 +37,21 @@ reg_multinomiale <- function(X, y, nb_iters, alpha,
                                 penalty = penalty,
                                 lambda = lambda,
                                 l1_ratio = l1_ratio)
-    theta[, k] <- result$theta
+    result$theta
   }
-
+  
+  # Stop the cluster
+  parallel::stopCluster(cl)
+  
+  # Combine results into the theta matrix
+  theta <- matrix(unlist(theta_list), nrow = n, byrow = FALSE)
+  colnames(theta) <- classes_uniques
+  rownames(theta) <- colnames(X_new)
+  
   return(theta)
+<<<<<<< Updated upstream
 }
+=======
+}
+
+>>>>>>> Stashed changes
