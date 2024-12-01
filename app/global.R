a0c284bc34f04aeb0074cd0ff3e0cd318ef6b0ed
Souraya Ahmed <99414919+Sahm269@users.noreply.github.com>
2024-12-01 02:30:10 +0100

    Application finie ,
    Mettre a jour bos noms et mail mais facultative

--- app/global.R
+++ app/global.R
@@ -3,6 +3,7 @@ library(shiny)             # Interface principale
 library(shinydashboard)    # Mise en page avancée
 library(shinyWidgets)      # Widgets stylisés supplémentaires
 
+
 # Bibliothèques pour la gestion des données
 library(readr)             # Lecture des fichiers CSV
 library(readxl)            # Lecture des fichiers Excel
@@ -14,9 +15,8 @@ library(LogisticRegression)
 
 # Bibliothèques pour les visualisations
 library(ggplot2)           # Graphiques de qualité
-library(plotROC)           # Courbes ROC
 library(caret)             # Partition des données, métriques de performance
-library(pROC)              # Analyse ROC (optionnel, complémentaire à plotROC)
+
 
 # Bibliothèques pour les tableaux et les téléchargements
 library(DT)                # Tableaux interactifs
@@ -26,5 +26,4 @@ library(writexl)           # Exportation des fichiers Excel
 # Bibliothèques pour les fonctionnalités supplémentaires
 library(shinyjs)           # Interactions JavaScript avec Shiny
 library(shinyalert)        # Notifications et alertes stylisées
-library(plotly)            # Graphiques interactifs
 library(shinycssloaders)
